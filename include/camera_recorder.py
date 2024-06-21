a3042874c4f9205a8e5fb1b4f40f8814bf1a7d4a
Mathias Pechinger <mathias.pechinger@gmail.com>
2024-06-21 14:17:56 +0200

    minor fix for image storage

--- include/camera_recorder.py
+++ include/camera_recorder.py
@@ -4,6 +4,7 @@ import datetime
 class CameraRecorder:
     def __init__(self, camera_index=0):
         self.camera = cv2.VideoCapture(camera_index)
+        self.counter = 0
 
     def record(self, scheduler_camera):
         ret, frame = self.camera.read()
@@ -15,9 +16,11 @@ class CameraRecorder:
         cv2.imwrite(f"static/cameraImages/latest/lastFrame.jpg", frame)
         
         # Save an image to cameraimages/storage every hour
-        current_time = datetime.datetime.now()
-        if current_time.minute == 0:  # Every hour
+        self.counter += 1
+        if self.counter >= 3600:  # Every hour (60 minutes)
+            current_time = datetime.datetime.now()
             storage_path = f"static/cameraImages/storage/{current_time.strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
             cv2.imwrite(storage_path, frame)
+            self.counter = 0
         
         scheduler_camera.enter(1, 1, self.record, (scheduler_camera,))
