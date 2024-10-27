c64e3243ec1a5daca893a8e8ebc6c52b79209e64
mathias pechinger <mathias.pechinger@gmail.com>
2024-10-27 10:08:51 +0100

    implemented firebase backend

--- include/plantgeek_backend_connector.py
+++ include/plantgeek_backend_connector.py
@@ -1,18 +1,17 @@
 import requests
 import urllib3
 from requests.exceptions import Timeout, ConnectionError
+from datetime import datetime
 
 # Disable SSL warnings for self-signed certificates (development only, TODO!!!!) 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class PlantGeekBackendConnector:
     def __init__(self):
-        self.login_url = 'https://plantgeek.de:5000/login'
-        self.data_url = 'https://plantgeek.de:5000/api/data'
-        self.image_url = 'https://plantgeek.de:5000/api/image'
+        self.data_url = 'https://writesysteminfo-jimd7cggoa-uc.a.run.app'
         self.credentials = {
-            'username': 'mathiaspechinger_iut8g5oo',
-            'api_key': '2114aadec64001c77d394845971f63dc'
+            'username': 'undefined',
+            'api_key': 'undefined'
         }
         self.timeout = 10  # Set a timeout of 10 seconds
 
@@ -23,68 +22,70 @@ class PlantGeekBackendConnector:
         }
 
     def sendImageToPlantGeekBackend(self, sc):
-        try:
-            # Send the login request with timeout
-            login_response = requests.post(self.login_url, json=self.credentials, verify=False, timeout=self.timeout)
+        # try:
+        #     # Send the login request with timeout
+        #     login_response = requests.post(self.login_url, json=self.credentials, verify=False, timeout=self.timeout)
 
-            if login_response.status_code == 200:
-                access_token = login_response.json().get('access_token')
-                file_path = 'static/cameraImages/latest/lastFrame.jpg'
-                files = {'file': open(file_path, 'rb')}
-                headers = {'Authorization': f'Bearer {access_token}'}
+        #     if login_response.status_code == 200:
+        #         access_token = login_response.json().get('access_token')
+        #         file_path = 'static/cameraImages/latest/lastFrame.jpg'
+        #         files = {'file': open(file_path, 'rb')}
+        #         headers = {'Authorization': f'Bearer {access_token}'}
 
-                # Send the image with timeout
-                data_response = requests.post(self.image_url, headers=headers, files=files, verify=False, timeout=self.timeout)
+        #         # Send the image with timeout
+        #         data_response = requests.post(self.image_url, headers=headers, files=files, verify=False, timeout=self.timeout)
 
-                if data_response.status_code != 200:
-                    print(f'Failed to send data. Status code: {data_response.status_code}')
-                    print('Response:', data_response.text)
-            else:
-                print(f'Failed to log in. Status code: {login_response.status_code}')
-                print('Response:', login_response.text)
+        #         if data_response.status_code != 200:
+        #             print(f'Failed to send data. Status code: {data_response.status_code}')
+        #             print('Response:', data_response.text)
+        #     else:
+        #         print(f'Failed to log in. Status code: {login_response.status_code}')
+        #         print('Response:', login_response.text)
                 
-        except (Timeout, ConnectionError) as e:
-            print(f"Connection error or timeout: {str(e)}")
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-        finally:
-            sc.enter(300, 1, self.sendImageToPlantGeekBackend, (sc,))
+        # except (Timeout, ConnectionError) as e:
+        #     print(f"Connection error or timeout: {str(e)}")
+        # except Exception as e:
+        #     print(f"An error occurred: {str(e)}")
+        # finally:
+        
+        sc.enter(300, 1, self.sendImageToPlantGeekBackend, (sc,))
 
     def sendDataToPlantGeekBackend(self, sc, sensorData, mqtt_interface):
         try:
-            # Send the login request with timeout
-            login_response = requests.post(self.login_url, json=self.credentials, verify=False, timeout=self.timeout)
+            
+            if sensorData.currentTemperature is None or sensorData.currentHumidity is None or sensorData.currentCO2 is None:
+                print('Data not ready yet')
+                sc.enter(5, 1, self.sendDataToPlantGeekBackend, (sc, sensorData, mqtt_interface,))
+                return
 
-            if login_response.status_code == 200:
-                access_token = login_response.json().get('access_token')
+            # Headers including the API key
+            headers = {
+                "Content-Type": "application/json",
+                "x-api-key": self.credentials['api_key'],
+                "x-user-id": self.credentials['username']
+            }
 
-                if sensorData.currentTemperature is None or sensorData.currentHumidity is None or sensorData.currentCO2 is None:
-                    print('Data not ready yet')
-                    sc.enter(5, 1, self.sendDataToPlantGeekBackend, (sc, sensorData, mqtt_interface,))
-                    return
+            # Data payload with extended values
+            data = {
+                "temperature": sensorData.currentTemperature,  # example temperature value
+                "co2ppm": sensorData.currentCO2,      # example CO2 ppm value
+                "humidity": sensorData.currentHumidity,     # example humidity valuecu
+                "light_state": mqtt_interface.getLightState(),  # example state boolean
+                "fridge_state": mqtt_interface.getFridgeState(),
+                "co2valve_state": mqtt_interface.getCO2State(),
+                "timestamp": datetime.utcnow().isoformat(),
+                "device_name": "device_1234"  # example device ID/name
+            }
 
-                data = {
-                    'username': self.credentials['username'],
-                    'api_key': self.credentials['api_key'],
-                    'temperature_c': sensorData.currentTemperature,
-                    'humidity': sensorData.currentHumidity,
-                    'co2': sensorData.currentCO2,
-                    'light_state': mqtt_interface.getLightState(),
-                    'fridge_state': mqtt_interface.getFridgeState(),
-                    'co2_state': mqtt_interface.getCO2State(),
-                }
-                headers = {'Authorization': f'Bearer {access_token}'}
+            # Send the POST request
+            response = requests.post(self.data_url, headers=headers, json=data)
 
-                # Send the data with timeout
-                data_response = requests.post(self.data_url, json=data, headers=headers, verify=False, timeout=self.timeout)
+            # Print the response (for debugging)
+            # if response.status_code == 200:
+            #     print("Success:", response.text)
+            # else:
+            #     print("Failed:", response.status_code, response.text)
 
-                if data_response.status_code != 200:
-                    print(f'Failed to send data. Status code: {data_response.status_code}')
-                    print('Response:', data_response.text)
-            else:
-                print(f'Failed to log in. Status code: {login_response.status_code}')
-                print('Response:', login_response.text)
-                
         except (Timeout, ConnectionError) as e:
             print(f"Connection error or timeout: {str(e)}")
         except Exception as e:
